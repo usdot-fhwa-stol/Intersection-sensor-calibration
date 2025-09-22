a1dfc400264e43cf82cf046129cf366712f22e15
mccullochnick <108553076+mccullochnick@users.noreply.github.com>
2025-09-22 10:10:45 -0400

    fixing original copy from ISC repo

--- scripts/FLIR/histEqualizeImage.m
+++ scripts/FLIR/histEqualizeImage.m
@@ -1,1 +1,20 @@
-404: Not Found
+%Read in input image and get mean/std dev of image
+clc; clear all; close all;
+
+allPngs = dir('/home/abey/Downloads/ThermalCamera4/flir-183/*.png*');
+
+for i=1:numel(allPngs)
+    file = allPngs(i).name;
+    fileName = split(file, ".");
+
+    inputFilename = strcat("/home/abey/Downloads/ThermalCamera4/flir-183/",fileName(1), ".png");
+
+    inputImg = imread(inputFilename);
+
+    %Perform histogram equalization and save image
+    outputImg = histeq(inputImg);
+    outputFilename = strcat("/home/abey/Downloads/ThermalCamera4/flir-183/",fileName(1), "_equalized.png");
+    imwrite(outputImg,outputFilename, 'png');
+
+
+end
--- scripts/FLIR/sharpenImage.m
+++ scripts/FLIR/sharpenImage.m
@@ -1,1 +1,12 @@
-404: Not Found
+clear all; clc; close all;
+
+%Median Filter Section
+inputImg = imread('flir180.png');
+
+%Iterate with filter size of 3,5,7
+for filterSize=3:2:7
+    %Filter the image with builtin imfilter function
+    averageFilteredImage = medfilt2(inputImg, [filterSize filterSize], "symmetric");
+    outputFilename = "flir180_" + filterSize + "x" + filterSize + "_medianfilter.png";
+    imwrite(averageFilteredImage,outputFilename, 'png');
+end
